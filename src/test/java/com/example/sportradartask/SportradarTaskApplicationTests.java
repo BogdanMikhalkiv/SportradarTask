afe56f5edd3ce3ce9e30df352f6f9d10ebb32282
BogdanMikhalkiv <bogdan.mikhalkiv1@gmail.com>
2026-03-20 12:09:46 +0100

    first test commit

---
+++ src/test/java/com/example/sportradartask/SportradarTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportradartask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportradarTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
