a98316e4060e381383198d3ad20a798b7d17087f
BogdanMikhalkiv <bogdan.mikhalkiv1@gmail.com>
2026-03-20 12:11:58 +0100

    first test commit

--- src/main/java/com/example/sportradartask/SportradarTaskApplication.java
+++ src/main/java/com/example/sportradartask/SportradarTaskApplication.java
@@ -9,6 +9,7 @@ public class SportradarTaskApplication {
     public static void main(String[] args) {
         SpringApplication.run(SportradarTaskApplication.class, args);
         //Hello World
+        //
     }
 
 }
